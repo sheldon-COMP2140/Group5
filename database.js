251cd53d4c1f7da1e3377a38e6b9dc0ce92d88c3
Thorn Brooks <95bthorn@gmail.com>
2024-11-29 02:04:48 -0500

    Add

--- database.js
+++
@@ -1,8 +0,0 @@
-// database.js
-const mysql = require('mysql2');
-require('dotenv').config();  // Load environment variables
-
-// Setup the database connection pool
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
